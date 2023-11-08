9afb6324b5c2888537a3346a15fa493ba6a3c83d
nahafatmaa <nahafatma@gmail.com>
2023-11-08 18:05:15 -0500

    Added the github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        This project is coded by Naha Fatma and is {""}
+        <a
+          href="https://github.com/nahafatmaa/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on github
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
