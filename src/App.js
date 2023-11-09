32cad32b4b35134361a41d2e79b60ab20756d8ec
nahafatmaa <nahafatma@gmail.com>
2023-11-09 18:30:07 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        This project is coded by Naha Fatma and is {""}
-        <a
-          href="https://github.com/nahafatmaa/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on github
-        </a>
-      </p>
+      <div className="container">
+        Weather App
+        <p>
+          This project is coded by Naha Fatma and is {""}
+          <a
+            href="https://github.com/nahafatmaa/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
