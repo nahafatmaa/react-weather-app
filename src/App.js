f0c2f8ab11a85d91cc7cd50d0de0241a572a447f
nahafatmaa <nahafatma@gmail.com>
2023-11-09 19:15:57 -0500

    added HTML for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <p>
           This project is coded by Naha Fatma and is {""}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search a City"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Weednesday 7:00pm</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="mostly cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li> precipitation: 15%</li>
+            <li> Humidity 12%</li>
+            <li>Wind 13 km/h </li>
+          </ul>
+        </div>
+      </div>
+      Hello from weather
+    </div>
+  );
+}
