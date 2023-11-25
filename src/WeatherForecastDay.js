31bf29f5431f2ca04d0bd656d007030ae0e69d33
nahafatmaa <nahafatma@gmail.com>
2023-11-25 17:05:46 -0500

    made updates to WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -19,7 +19,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.weather[0].weather[0].icon} size={36} />
+      <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
